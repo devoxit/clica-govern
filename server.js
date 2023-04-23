c2f60cc0d1c25e86bc5fc9c95528c838d6f99150
ymran <ymran.fatih@gmail.com>
2023-04-24 05:59:33 +0900

    port

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 const app = require('./app/main')
 const connectDB = require('./db/config')
-const port = process.env.PORT ||8000
+const port = process.env.PORT || 6060
 require('dotenv').config()
 
 const opt = {
-	useUnifiedTopology : true,
-	useNewUrlParser : true
+    useUnifiedTopology: true,
+    useNewUrlParser: true
 }
 // app.listen(port,()=>{
 //     console.log("server listening at port:", port)
 // })
 //DBconnecting
 const start = async () => {
-    try{
-await connectDB(process.env.MONGO_URL,opt)
-app.listen(port,()=>{
-    console.log("server listening at port:", port)
-})
-    }catch(err) {
+    try {
+        await connectDB(process.env.MONGO_URL, opt)
+        app.listen(port, () => {
+            console.log("server listening at port:", port)
+        })
+    } catch (err) {
         console.log(err)
     }
 }
